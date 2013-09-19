2875baf27865929f0daabfc22d497e6a76bd02b5
Sandreu <sebastien.andreu@gmail.com>
2013-09-19 14:39:24 +0200

    Parse

--- cake-model.js
+++ cake-model.js
@@ -3,6 +3,26 @@ Backbone.CakeModel = Backbone.RelationalModel.extend({
     cakeModel: false,
     cakeController: false,
     rest : true,
+    constructor: function(attrinutes, options) {
+        options = _.defaults(
+            options || {},
+            { parse: true }
+        );
+        Backbone.RelationalModel.call(this, attrinutes, options);
+    },
+    parse: function (response, options) {
+        var data;
+        if (isset(response.data)) data = response.data;
+        else data = response;
+        
+        if (_.isString(this.cakeModel)) {
+            if (!_.isEmpty(data[this.cakeModel])) {
+                data = _.extend(data, data[this.cakeModel]);
+                delete data[this.cakeModel];
+            }
+        }
+        return data;
+    },
     urlRoot: function () {
         if (_.isString(this.cakeController)) return app.root + this.cakeController + '/rest';
         return false;
@@ -13,7 +33,6 @@ Backbone.CakeModel = Backbone.RelationalModel.extend({
         return Backbone.sync.apply(this, arguments);
     },
     set: function( key, value, options ) {
-
         // Duplicate backbone's behavior to allow separate key/value parameters, instead of a single 'attributes' object
         var attributes;
         if (_.isObject( key ) || key == null) {
@@ -38,10 +57,6 @@ Backbone.CakeModel = Backbone.RelationalModel.extend({
       var ajax = Backbone.RelationalModel.prototype.save.apply(this, arguments);
       if (!ajax) return ajax;
       else return ajax.success($.proxy(function (data) { this.trigger('saved', this, data); }, this));
-    },
-    parse: function (response) {
-      if (isset(response.data)) return response.data;
-      else return response;
     }
 });
 
